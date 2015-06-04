cd7b4fc3994f70f6d2263f9f589a517815173bdc
Colby Somerville <colby.somerville@gmail.com>
2015-06-04 12:11:59 -0400

    map view created

--- app/assets/javascripts/backbone/router/router.js
+++ app/assets/javascripts/backbone/router/router.js
@@ -11,5 +11,10 @@ Arbor.Router = Backbone.Router.extend({
     var searchForm = new Arbor.Views.SearchForm();
     searchForm.render();
     $('body').append(searchForm.el);
+    var map = new Arbor.Views.Map();
+    map.$el.attr("height", "100%")
+    $('html').attr("height", "100%")
+    $('body').attr("height", "100%")
+    $('body').append(map.el);
   }
 })
---
+++ app/assets/javascripts/backbone/views/map.js
@@ -0,0 +1,15 @@
+var Arbor = Arbor || { Models: {}, Collections: {}, Views: {} };
+
+Arbor.Views.Map = Backbone.View.extend({
+
+  initialize: function(){
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(40.7127, -74.0059)
+    }
+    new google.maps.Map(this.el, mapOptions)
+  },
+
+  id: "map-canvas"
+
+})
