d573fe4025b2df44fbf6d922f103df90642d5a4b
Colby Somerville <colby.somerville@gmail.com>
2015-06-09 07:41:11 -0400

    remembered to comment out Tree.delete_all jit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Tree.delete_all
+# Tree.delete_all
 
 species_code_key = {}
 txt = File.read('./db/raw_species.txt')
