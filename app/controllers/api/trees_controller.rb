1acb15ffe9fce474440535f2e7be38a16f5cafec
Colby Somerville <colby.somerville@gmail.com>
2015-07-09 10:25:33 -0400

    more tree model refactoring

--- app/controllers/api/trees_controller.rb
+++ app/controllers/api/trees_controller.rb
@@ -2,7 +2,11 @@ module Api
   class TreesController < ApplicationController
 
     def index
-      render json: Tree.search(params)
+      render json: Tree.search(params) if coords?(params)
+    end
+
+    def coords?(params)
+      params[:n].present? && params[:s].present? && params[:e].present? && params[:w].present?
     end
 
   end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -3,14 +3,20 @@ class Tree < ActiveRecord::Base
   has_many :trails, through: :stops
 
   def self.search(params)
-    query = Tree.all
-
-    query = query.where(lat: params[:s]..params[:n]).where(long: params[:w]..params[:e]).take(100)
-
-    query = query.where(species: params[:species]) if params[:species].present?
-
-    query = query.where(diameter: params[:diameter]) if params[:diameter].present?
-
-    query
+    with_species(params[:species])
+      .with_diameter(params[:diameter])
+      .where(lat: params[:s]..params[:n]).where(long: params[:w]..params[:e]).take(100)
   end
+
+  scope :with_species, ->(species) {
+    if species.present?
+      where(species: species)
+    end
+  }
+
+  scope :with_diameter, ->(diameter) {
+    if diameter.present?
+      where(species: species)
+    end
+  }
 end
